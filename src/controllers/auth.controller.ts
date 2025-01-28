9affd196e9db7c93ee63269edd8b88380d97aae3
Faizan <faizanms025347@gmail.com>
2025-01-28 17:42:10 +0530

    updated url

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -188,8 +188,8 @@ exports.googleCallback = [
         });
 
         console.log('Successfully authenticated, redirecting...');
-        res.redirect('http://localhost:5173/');
-        // res.redirect('http://localhost:5173/dashboard');
+        // res.redirect('http://localhost:5173/');
+        res.redirect('http://dev-blogg.vercel.app');
       } catch (error) {
         console.error('Google callback error:', error);
         res.status(500).json({ message: 'Internal server error.' });
